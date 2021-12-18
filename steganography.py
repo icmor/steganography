a6351af417e8820fd581f8eb0c4ff4f0e405fdbe
icmor <cornejodlm@ciencias.unam.mx>
2021-12-17 23:46:12 -0500

    Merge two images together and unmerge them
    Not the prettiest code I'll admit but it was pretty fun to write.
    
    The dimensions for the hidden image are hidden in the first pixels
    of the merged image. The first byte holds in its last 4 bits (nibble)
    the bytes each dimension takes up (0-15). If it has a value of 3
    for example, then you will find the width in the next 3 bytes
    (in their lower nibbles) and height in the 3 after those.

--- steganography.py
+++ steganography.py
@@ -1,83 +1,117 @@
 from PIL import Image
+from math import ceil
 
 class Steganography:
-    def __init__(self, image):
-        self.image = image
-        self.px = image.load()
-
+    def __init__(self, path):
+        self.image = Image.open(path)
+        self.image.close()
 
     def pixel_merge(self, p1, p2):
-        return (p1[0] & 240 | (p2[0] & 240) >> 4,
-                p1[1] & 240 | (p2[1] & 240) >> 4,
-                p1[2] & 240 | (p2[2] & 240) >> 4)
+        result = tuple()
+        for i, j in zip(p1, p2):
+            result += (i & 0b11110000| (j & 0b11110000) >> 4,)
+        return tuple(result)
 
-    def encode_size(self):
-        self.px[0,0] = ((self.image.width & 15) << 4,
-                       (self.image.width & 240),
-                        (self.image.width & 3840) >> 4)
-        self.px[0,1] = ((self.image.width & 61440) >> 8,
-                       (self.image.height & 15) << 4,
-                       (self.image.height & 240))
-        self.px[0,2] = ((self.image.height & 3840) >> 4,
-                       (self.image.height & 61440) >> 8,
-                       self.px[0, 2][2])
+    def normalize_modes(self, other):
+        if self.image.mode == other.image.mode:
+            return
+        elif self.image.getbands() > other.image.getbands():
+            with Image.open(other.image.filename) as img:
+                other.image = img.convert(self.image.mode)
+        else:
+            with Image.open(self.image.filename) as img:
+                self.image = img.convert(other.image.mode)
+
+    def encode_size(self, img):
+        # Get how many chunks of four bits are needed to store
+        # each dimension
+        nibbles = ceil(len(bin(max(img.size))[2:]) / 4)
+        size = self.image.size[1] << nibbles * 4 | self.image.size[0]
+        bands = len(img.getbands())
+        mask = 0b1111
+        flag = False
+        px = img.load()
+        for i in range(ceil((nibbles * 2 + 1) / bands)):
+            colors = tuple()
+            for j in range(bands):
+                if not i and not j:
+                    colors += (nibbles << 4,)
+                elif flag:
+                    colors += (px[0, i][j],)
+                else:
+                    shift = len(bin(mask)[2:])
+                    if shift > 8:
+                        colors += ((size & mask) >> shift - 8,)
+                    else:
+                        colors += ((size & mask) << 8 - shift,)
+                    mask <<= 4
+                    if mask > 2 ** (nibbles * 8):
+                        flag = True
+            px[0, i] = colors
+            if flag: break
+        return img
 
     def decode_size(self):
-        width = self.px[0,0][0] & 15
-        width += (self.px[0,0][1] & 15) << 4
-        width += (self.px[0,0][2] & 15) << 8
-        width += (self.px[0,1][0] & 15) << 12
-        height = self.px[0,1][1] & 15
-        height += (self.px[0,1][2] & 15) << 4
-        height += (self.px[0,2][0] & 15) << 8
-        height += (self.px[0,2][1] & 15) << 12
-        return (width, height)
+        with Image.open(self.image.filename) as img:
+            px = img.load()
+            nibbles = 0b1111 & px[0, 0][0]
+            bands = len(self.image.getbands())
+            acc = ''
+            for i in range(ceil((nibbles * 2 + 1) / bands)):
+                for j in range(bands):
+                    if not i and not j: continue
+                    if i * bands + j >= nibbles * 2 + 1:
+                        return (int(acc[nibbles * 4:], 2),
+                                int(acc[:nibbles * 4], 2))
+                    acc = "{:04b}".format(px[0, i][j] & 15) + acc
 
     def merge(self, other):
         if self.image.width * self.image.height \
            <  other.image.width * other.image.height:
             print("Error: hidden image can't be bigger than source image.")
-            return None
-        else:
-            other.encode_size()
-            out = Steganography(Image.new('RGB', self.image.size))
+            return
+        self.normalize_modes(other)
+        out = Image.new(self.image.mode, self.image.size)
+        with Image.open(self.image.filename) as img1, \
+             Image.open(other.image.filename) as img2:
+            other.encode_size(img2)
+            pxout = out.load()
+            px1 = img1.load()
+            px2 = img2.load()
+            w2 = h2 = 0
+            for w1 in range(self.image.width):
+                 for h1 in range(self.image.height):
+                    if w2 == other.image.width:
+                        pxout[w1, h1] = px1[w1, h1]
+                    else:
+                        pxout[w1, h1] = self.pixel_merge(px1[w1, h1], px2[w2, h2])
+                        h2 += 1
+                        if h2 == other.image.height:
+                            h2 = 0
+                            w2 += 1
+        out.save("merged_image.png")
+        out.close()
+
+    def unmerge(self):
+        with Image.open(self.image.filename) as img:
+            out = Image.new(self.image.mode, self.decode_size())
+            pxout = out.load()
+            px = img.load()
             w2 = h2 = 0
             for w1 in range(self.image.width):
                 for h1 in range(self.image.height):
-                    if h2 == other.image.height:
+                    if w2 == out.width: break
+                    colors = tuple()
+                    colors_out = tuple()
+                    for color in px[w1, h1]:
+                        colors += (color & 0b11110000,)
+                        colors_out += ((color & 0b1111 ) << 4,)
+                    px[w1, h1] = colors
+                    pxout[w2, h2] = colors_out
+                    h2 += 1
+                    if h2 == out.height:
                         h2 = 0
                         w2 += 1
-                    if w2 == other.image.width:
-                        out.px[w1, h1] = self.px[w1, h1]
-                    else:
-                        out.px[w1, h1] = self.pixel_merge(self.px[w1, h1],
-                                                          other.px[w2, h2])
-                        h2 += 1
-            return out
-
-    def unmerge(self):
-        out = Steganography(Image.new('RGB', self.decode_size()))
-        width, height = self.decode_size()
-        w2 = h2 = 0
-        for w1 in range(self.image.width):
-            for h1 in range(self.image.height):
-                if h2 == height:
-                    h2 = 0
-                    w2 += 1
-                if w2 == width:
-                    break
-                out.px[w2, h2] = ((self.px[w1, h1][0] & 15) << 4,
-                               (self.px[w1, h1][1] & 15) << 4,
-                               (self.px[w1, h1][2] & 15) << 4)
-                self.px[w1, h1] = ((self.px[w1, h1][0] & 240),
-                                      (self.px[w1, h1][1] & 240),
-                                      (self.px[w1, h1][2] & 240))
-                h2 += 1
-        return out
-# def main():
-image = Steganography(Image.open("anon.jpg"))
-hidden = Steganography(Image.open("cove.jpg"))
-target = image.merge(hidden)
-target.image.save('merged.jpg')
-new = target.unmerge()
-new.image.save('unmerged.jpg')
+            img.save("decoded_" + self.image.filename)
+        out.save("hidden.png")
+        out.close()
